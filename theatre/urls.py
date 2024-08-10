54c8aaafc2289cd911beb0a1f0f8a58f6fd6d646
Anton Korinchuk <antonkorinchuk@gmail.com>
2024-08-10 18:45:15 +0300

    Added endpoints for plays, performances

---
+++ theatre/urls.py
@@ -0,0 +1,24 @@
+from django.urls import include, path
+from rest_framework import routers
+
+from theatre.views import (
+    GenreViewSet,
+    ActorViewSet,
+    TheatreHallViewSet,
+    PlayViewSet,
+    PerformanceViewSet,
+)
+
+router = routers.DefaultRouter()
+router.register("genres", GenreViewSet)
+router.register("actors", ActorViewSet)
+router.register("theatre_halls", TheatreHallViewSet)
+router.register("plays", PlayViewSet)
+router.register("performances", PerformanceViewSet)
+
+
+urlpatterns = [
+    path("", include(router.urls)),
+]
+
+app_name = "theatre"
