38e81c96ab2395e42e72b4a4aa846ac0369b7889
Anton Korinchuk <antonkorinchuk@gmail.com>
2024-08-10 16:31:03 +0300

    Fixed bags & added validation to models

--- theatre/models.py
+++ theatre/models.py
@@ -1,5 +1,6 @@
 from django.conf import settings
 from django.db import models
+from rest_framework.exceptions import ValidationError
 
 
 class Actor(models.Model):
@@ -7,6 +8,10 @@ class Actor(models.Model):
     last_name = models.CharField(max_length=100)
 
     def __str__(self) -> str:
+        return f"{self.first_name} {self.last_name}"
+
+    @property
+    def full_name(self) -> str:
         return f"{self.first_name} {self.last_name}"
 
 
@@ -20,18 +25,8 @@ class Genre(models.Model):
 class Play(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField()
-    actors = models.ManyToManyField(
-        Actor,
-        related_name="plays",
-        null=True,
-        blank=True
-    )
-    genres = models.ManyToManyField(
-        Genre,
-        related_name="genres",
-        null=True,
-        blank=True
-    )
+    actors = models.ManyToManyField(Actor, related_name="plays", blank=True)
+    genres = models.ManyToManyField(Genre, related_name="plays", blank=True)
 
     def __str__(self) -> str:
         return self.title
@@ -39,14 +34,10 @@ class Play(models.Model):
 
 class Performance(models.Model):
     play = models.ForeignKey(
-        Play,
-        on_delete=models.CASCADE,
-        related_name="performances"
+        Play, on_delete=models.CASCADE, related_name="performances"
     )
     theatre_hall = models.ForeignKey(
-        "TheatreHall",
-        on_delete=models.CASCADE,
-        related_name="performances"
+        "TheatreHall", on_delete=models.CASCADE, related_name="performances"
     )
     show_time = models.DateTimeField()
 
@@ -59,30 +50,65 @@ class TheatreHall(models.Model):
     def __str__(self) -> str:
         return self.name
 
+    @property
+    def capacity(self) -> int:
+        return self.rows * self.seats_in_row
+
 
 class Ticket(models.Model):
     row = models.IntegerField()
     seat = models.IntegerField()
     performance = models.ForeignKey(
-        Performance,
-        on_delete=models.CASCADE,
-        related_name="tickets"
+        Performance, on_delete=models.CASCADE, related_name="tickets"
     )
     reservation = models.ForeignKey(
-        "Reservation",
-        on_delete=models.CASCADE,
-        related_name="tickets"
+        "Reservation", on_delete=models.CASCADE, related_name="tickets"
     )
+
+    @staticmethod
+    def validate_ticket(row, seat, theatre_hall, error_to_raise):
+        for ticket_attr_value, ticket_attr_name, theatre_hall_attr_name in [
+            (row, "row", "rows"),
+            (seat, "seat", "seats_in_row"),
+        ]:
+            count_attrs = getattr(theatre_hall, theatre_hall_attr_name)
+            if not (1 <= ticket_attr_value <= count_attrs):
+                raise error_to_raise(
+                    {
+                        ticket_attr_name: f"{ticket_attr_name} "
+                        f"number must be in available range: "
+                        f"(1, {theatre_hall_attr_name}): "
+                        f"(1, {count_attrs})"
+                    }
+                )
+
+    def clean(self):
+        Ticket.validate_ticket(
+            self.row,
+            self.seat,
+            self.performance.theatre_hall,
+            ValidationError,
+        )
+
+    def save(self, *args, force_insert=False, force_update=False, using=None, update_fields=None):
+        self.full_clean()
+        return super(Ticket, self).save(
+            force_insert, force_update, using, update_fields
+        )
+
+    def __str__(self):
+        return f"{str(self.performance)} (row: {self.row}, seat: {self.seat})"
+
+    class Meta:
+        unique_together = ("performance", "row", "seat")
+        ordering = ["row", "seat"]
 
 
 class Reservation(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     user = models.ForeignKey(
-        settings.AUTH_USER_MODEL,
-        on_delete=models.CASCADE,
-        related_name="reservations"
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="reservations"
     )
 
     def __str__(self) -> str:
         return str(self.created_at)
-
